7de3081a813286054436b23c20754edd85b70778
John Yoder <jcyoder@gmail.com>
2013-08-25 10:24:39 -0700

    Add some comments to upcase-alternative.cpp

--- lessons/001/source/upcase-alternative.cpp
+++ lessons/001/source/upcase-alternative.cpp
@@ -1,3 +1,10 @@
+// This is a slight simplification of upcase.cpp which avoids storing words in a
+// vector by progressively building the next line of output. It determines when
+// the line is complete and should be printed by tracking the current column
+// number. When the column reaches a number evenly divisible by 8 (by the use of
+// the modulous operator: "%"), it's time to print the line.
+
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,12 +15,15 @@ int main () {
     int column = 1;
     std::string line;
     std::string nextWord;
-    while (std::cin >> nextWord && nextWord != "q") {
+    // Loop, accepting words from the standard input.
+    while (std::cin >> nextWord) {
         for (auto& letter : nextWord) {
             letter = toupper(letter);
         }
+        // Shove the next word onto the line we're building.
         line.append(nextWord);
 
+        // Determine when it's time to print the line.
         if (column % 8 == 0) {
             std::cout << line << std::endl;
             line.clear();
@@ -22,6 +32,7 @@ int main () {
         }
         column++;
     }
+    // Generate a final newline if necessary.
     if (!line.empty()) {
         std::cout << line << std::endl;
     }
